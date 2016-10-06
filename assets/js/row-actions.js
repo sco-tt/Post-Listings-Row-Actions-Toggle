e058eb6418d7eee8b0e40f140ed5688cba2932b4
Scott Pinkelman <scott@scottpinkelman.com>
2016-10-06 12:40:24 -0600

    Removes commented code; improves click handler to target link

--- assets/js/row-actions.js
+++ assets/js/row-actions.js
@@ -1,14 +1,11 @@
 (function($) {
     'use strict';
     $(document).ready(function() {
-        var $rowActionsTrigger = $('.row-actions .show-actions-trigger');
+        var $rowActionsTrigger = $('.row-actions .show-actions-trigger a');
 
-        // $rowActionsTrigger.each(function() {
-        //     $(this).html('<a href="#">Show Actions</a>   ');
-        // })
-
-        $rowActionsTrigger.on('click', function() {
-            var $rowActions = $(this).parent();
+        $rowActionsTrigger.on('click', function(e) {
+            e.preventDefault();
+            var $rowActions = $(this).closest('.row-actions');
             $rowActions.removeClass('visible');
             $rowActions.children(':gt(0)').toggleClass('hide-row-actions');
         });
