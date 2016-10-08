1842a0dccc5ff9010add0d0b3b02a7d81689abbf
Scott Pinkelman <scott@scottpinkelman.com>
2016-10-07 18:17:10 -0600

    Event bubbling refactor for script

--- assets/js/row-actions.js
+++ assets/js/row-actions.js
@@ -1,10 +1,12 @@
 (function($) {
     'use strict';
     $(document).ready(function() {
-        var $rowActionsTrigger = $('.row-actions .show-actions-trigger a');
+        var $rowActionsTrigger = $('.row-actions' );
 
-        $rowActionsTrigger.on('click', function(e) {
+
+        $rowActionsTrigger.on('click', '.show-actions-trigger a', function(e) {
             e.preventDefault();
+            console.log($(this));
             var $rowActions = $(this).closest('.row-actions');
             $rowActions.removeClass('visible');
             $rowActions.children(':gt(0)').toggleClass('hide-row-actions');
